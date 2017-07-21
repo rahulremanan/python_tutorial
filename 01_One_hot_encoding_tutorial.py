837adf4e5877e29e934d8fd1a142d0eead02bedd
rahulremanan <rahul@remanan.net>
2017-07-21 17:26:07 -0400

    File name change.

---
+++ 01_One_hot_encoding_tutorial.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 15:26:57 2017
+
+@author: Rahul
+"""
+# One Hot Encoding Tutorial
+# Forked from: http://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
+
+from numpy import argmax
+# define input string
+data = 'hello world'
+print(data)
+# define universe of possible input values
+alphabet = 'abcdefghijklmnopqrstuvwxyz '
+# define a mapping of chars to integers
+char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+# integer encode input data
+integer_encoded = [char_to_int[char] for char in data]
+print(integer_encoded)
+
+# one hot encode
+onehot_encoded = list()
+for value in integer_encoded:
+	letter = [0 for _ in range(len(alphabet))]
+	letter[value] = 1
+	onehot_encoded.append(letter)
+print(onehot_encoded)
+# invert encoding
+inverted = int_to_char[argmax(onehot_encoded[0])]
+print(inverted)
+
+# one hot encode using sci-kit learn
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+# define example
+data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+values = array(data)
+print(values)
+# integer encode
+label_encoder = LabelEncoder()
+label_encoded = label_encoder.fit_transform(values)
+print(label_encoded)
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+label_encoded = label_encoded.reshape(len(label_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(label_encoded)
+print(onehot_encoded)
+# invert first example
+inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+print(inverted)
+
+# one hot encode using keras
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from keras.utils import to_categorical
+# define example
+data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+values = array(data)
+print(values)
+# integer encode
+label_encoder = LabelEncoder()
+label_encoded = label_encoder.fit_transform(values)
+print(label_encoded)
+# one hot encode
+encoded = to_categorical(label_encoded)
+print(encoded)
+# invert encoding
+label_encoded = argmax(encoded[0])
+inverted = label_encoder.inverse_transform(label_encoded)
+print(inverted)
+
+# one hot encode using keras for numerical categories
+
+from numpy import array
+from numpy import argmax
+from keras.utils import to_categorical
+# define example
+data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
+data = array(data)
+print(data)
+# one hot encode
+encoded = to_categorical(data)
+print(encoded)
+# invert encoding
+inverted = argmax(encoded[0])
+print(inverted)
