5643e85af67fe8772e0a4183892b798c9697e8ef
rahulremanan <rahul@remanan.net>
2017-08-26 16:50:27 -0400

    Added pre-trained model weights for single-supporting facts.

--- NLP/Week_06/src/06_QA_Demo.py
+++ NLP/Week_06/src/06_QA_Demo.py
@@ -1,22 +1,77 @@
 import keras
 from keras.models import Sequential, Model
 from keras.layers.embeddings import Embedding
-from keras.layers import Input, Activation, Dense, Permute, Dropout, add, concatenate, dot
+from keras.layers import Input, Activation, Dense, Permute, Dropout, add, concatenate, dot, TimeDistributed, Flatten, Merge
 from keras.layers import LSTM
+from keras.utils.data_utils import get_file
 from keras.preprocessing.sequence import pad_sequences
 from functools import reduce
 import tarfile
 import numpy as np
 import re
+import argparse
+import time
+import datetime as dt
+import os
+import glob
 
-train_model = 1
-train_epochs = 5000
-load_model = 0
-batch_size = 500
-lstm_size = 1024
-est_qualitative = 0
-user_questions = 0
-num_recurrent_units = 10
+class housekeeping:
+    def is_valid_file(parser, arg):
+        arg = arg.replace("'",'' )
+        arg = arg.replace('"', '')
+        if not os.path.isfile(arg):
+            parser.error("the file %s does not exist..." % arg)
+        else:
+            return arg
+    def is_valid_dir(parser, arg):
+        arg = arg.replace("'",'' )
+        arg = arg.replace('"', '')
+        if not os.path.isdir(arg):
+            parser.error("the folder %s does not exist..." % arg)
+        else:
+            return arg
+    def mkdate(datestr):
+        datestr = datestr.replace("'","")
+        datestr = datestr.replace('"','')
+        try:
+            datestr = dt.datetime.strptime(datestr, '%Y,%m,%d')
+        except:
+            datestr = dt.datetime.strptime(datestr, '%Y-%m-%d')
+        return datestr
+    def to_integer(dt_time):
+        return dt_time.year, dt_time.month, dt_time.day
+    def generate_timestamp():
+        timestamp=0
+        try:
+            if timestamp <1:
+                timestamp +=1
+                timestring = time.strftime("%Y_%m_%d-%H_%M_%S")
+                print ("time stamp generated: "+timestring)
+                return timestring
+            else:
+                timestamp+=1
+                print ("time stamp already generated: "+timestring)
+        except:
+            print ("error generating time stamp...")    
+    def create_data_dir(directory):
+         if not os.path.exists(directory):
+            os.makedirs(directory)
+
+train_model = 0
+train_epochs = 66
+load_model = 1
+batch_size = 750
+lstm_size = 32
+test_qualitative = 0
+user_questions = 1
+num_recurrent_units = 8
+dropout = 0.3
+timestr = housekeeping.generate_timestamp()
+dataset_loc = ('/home/info/babi_tasks_1-20_v1-2.tar.gz')
+weights_loc = ('/home/info/qa_demo_weights*.[Hh]5')
+stateful_weights_loc = '/home/info/qa_demo_weights_stateful*.[Hh]5'
+weights_save_loc = ('/home/info/qa_demo_weights_'+timestr+'.h5')
+stateful_weights_save_loc = ('/home/info/qa_demo_weights_stateful_'+timestr+'.h5')
 
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
@@ -24,7 +79,6 @@ def tokenize(sent):
     ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
     '''
     return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
-
 
 def parse_stories(lines, only_supporting=False):
     '''Parse stories provided in the bAbi tasks format
@@ -57,7 +111,6 @@ def parse_stories(lines, only_supporting=False):
             story.append(sent)
     return data
 
-
 def get_stories(f, only_supporting=False, max_length=None):
     '''Given a file name, read the file,
     retrieve the stories,
@@ -69,7 +122,6 @@ def get_stories(f, only_supporting=False, max_length=None):
     flatten = lambda data: reduce(lambda x, y: x + y, data)
     data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
     return data
-
 
 def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     X = []
@@ -87,7 +139,14 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     return (pad_sequences(X, maxlen=story_maxlen),
             pad_sequences(Xq, maxlen=query_maxlen), np.array(Y))
 
-tar = tarfile.open('/home/info/babi_tasks_1-20_v1-2.tar.gz')
+#try:
+#    path = get_file('babi-tasks-v1-2.tar.gz', origin='https://github.com/rahulremanan/python_tutorial/blob/master/NLP/data/babi_tasks_1-20_v1-2.tar.gz')
+#except:
+#    print('Error downloading dataset, please download it manually:\n'
+#          '$ wget https://github.com/rahulremanan/python_tutorial/blob/master/babi_tasks_1-20_v1-2.tar.gz'
+#          '$ mv tasks_1-20_v1-2.tar.gz ~/.keras/datasets/babi_tasks_v1-2.tar.gz')
+#    raise
+tar = tarfile.open(dataset_loc)
 
 challenges = {
     # QA1 with 10,000 samples
@@ -150,38 +209,28 @@ print('answers_test shape:', answers_test.shape)
 print('-')
 print('Compiling...')
 
-# placeholders
 input_sequence = Input((story_maxlen,))
 question = Input((query_maxlen,))
 
 print('Input sequence:', input_sequence)
 print('Question:', question)
 
-# encoders
-# embed the input sequence into a sequence of vectors
 input_encoder_m = Sequential()
 input_encoder_m.add(Embedding(input_dim=vocab_size,
                               output_dim=64))
-input_encoder_m.add(Dropout(0.3))
-# output: (samples, story_maxlen, embedding_dim)
+input_encoder_m.add(Dropout(dropout))
 
-# embed the input into a sequence of vectors of size query_maxlen
 input_encoder_c = Sequential()
 input_encoder_c.add(Embedding(input_dim=vocab_size,
                               output_dim=query_maxlen))
-input_encoder_c.add(Dropout(0.3))
-# output: (samples, story_maxlen, query_maxlen)
+input_encoder_c.add(Dropout(dropout))
 
-# embed the question into a sequence of vectors
 question_encoder = Sequential()
 question_encoder.add(Embedding(input_dim=vocab_size,
                                output_dim=64,
                                input_length=query_maxlen))
-question_encoder.add(Dropout(0.3))
-# output: (samples, query_maxlen, embedding_dim)
+question_encoder.add(Dropout(dropout))
 
-# encode input sequence and questions (which are indices)
-# to sequences of dense vectors
 input_encoded_m = input_encoder_m(input_sequence)
 print('Input encoded m', input_encoded_m)
 input_encoded_c = input_encoder_c(input_sequence)
@@ -189,26 +238,30 @@ print('Input encoded c', input_encoded_c)
 question_encoded = question_encoder(question)
 print('Question encoded', question_encoded)
 
-
-# compute a 'match' between the first input vector sequence
-# and the question vector sequence
-# shape: `(samples, story_maxlen, query_maxlen)
 match = dot([input_encoded_m, question_encoded], axes=(2, 2), normalize=False)
 match = Activation('softmax')(match)
 print('Match shape', match)
 
-# add the match matrix with the second input vector sequence
-response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+response = add([match, input_encoded_c]) 
+response = Permute((2, 1))(response) 
 print('Response shape', response)
 
-# concatenate the response vector with the question vector sequence
 answer = concatenate([response, question_encoded])
 print('Answer shape', answer)
 
+if load_model ==1:
+    try:
+        newest_weights = max(glob.iglob(weights_loc), 
+                         key=os.path.getctime)
+        newest_weights_stateful = max(glob.iglob(stateful_weights_loc), 
+                                  key=os.path.getctime)
+        print (newest_weights, newest_weights_stateful)
+    except:
+        print ('Error loading saved weights...')
+
 class RNN:
-    def LSTM_layer(answer, lstm_size, dropout, vocab_size):
-        answer = LSTM(lstm_size, return_sequences=True)(answer)
+    def LSTM_layer(input_sequence, question, answer, lstm_size, dropout, vocab_size, batch_size):
+        answer = LSTM(units=lstm_size, return_sequences=True)(answer)
         answer = Dropout(dropout)(answer)
         for cur_unit in range(num_recurrent_units):
             answer = LSTM(lstm_size, return_sequences=True)(answer)
@@ -221,18 +274,17 @@ class RNN:
               metrics=['accuracy'])
         return model
 
-dropout = 0.3
-model = RNN.LSTM_layer(answer, lstm_size, dropout, vocab_size)
-model.summary()
-
-if load_model == 1:
-    model = keras.models.load_model('/home/info/model_qa_demo.h5')
-
-if train_model == 1:
-    # train, batch_size = 32 and epochs = 120
-    model.fit([inputs_train, queries_train], answers_train, batch_size, train_epochs,
+try:
+    model = RNN.LSTM_layer(input_sequence, question, answer, lstm_size, dropout, vocab_size, batch_size)
+    model.summary()
+    if load_model == 1:
+        model = keras.models.load_model(newest_weights)
+    if train_model == 1:
+        model.fit([inputs_train, queries_train], answers_train, batch_size, train_epochs,
           validation_data=([inputs_test, queries_test], answers_test))
-    model.save('/home/info/model_qa_demo.h5')
+        model.save(weights_save_loc)
+except:
+    print ("Can't train the model...")
 
 if test_qualitative == 1:
     print('-------------------------------------------------------------------------------------------')
@@ -248,13 +300,16 @@ if user_questions == 1:
     print('-------------------------------------------------------------------------------------------')
     print('Custom User Queries (Make sure there are spaces before each word)')
     while 1:
-        print('-------------------------------------------------------------------------------------------')
-        print('Please input a story')
-        user_story_inp = input().split(' ')
-        print('Please input a query')
-        user_query_inp = input().split(' ')
-        user_story, user_query, user_ans = vectorize_stories([[user_story_inp, user_query_inp, '.']], word_idx, story_maxlen, query_maxlen)
-        user_prediction = model.predict([user_story, user_query])
-        user_prediction = idx_word[np.argmax(user_prediction)]
-        print('Result')
-        print(' '.join(user_story_inp), ' '.join(user_query_inp), '| Prediction:', user_prediction)
+        try:
+            print('-------------------------------------------------------------------------------------------')
+            print('Please input a story')
+            user_story_inp = input().split(' ')
+            print('Please input a query')
+            user_query_inp = input().split(' ')
+            user_story, user_query, user_ans = vectorize_stories([[user_story_inp, user_query_inp, '.']], word_idx, story_maxlen, query_maxlen)
+            user_prediction = model.predict([user_story, user_query])
+            user_prediction = idx_word[np.argmax(user_prediction)]
+            print('Result')
+            print(' '.join(user_story_inp), ' '.join(user_query_inp), '| Prediction:', user_prediction)
+        except:
+            print ('Question cannot be understood. Please ensure spaces between each words...')
