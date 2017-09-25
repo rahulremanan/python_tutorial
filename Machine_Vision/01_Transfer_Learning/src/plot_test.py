65ae375532f14e55d9a52ea391df5ac1f51f0457
rahulremanan <rahul@remanan.net>
2017-09-25 13:29:42 -0400

    Stylistic changes

--- Machine_Vision/01_Transfer_Learning/src/plot_test.py
+++ Machine_Vision/01_Transfer_Learning/src/plot_test.py
@@ -7,26 +7,6 @@ import matplotlib.pyplot as plt
 import numpy
 import os
 import time
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-# load pima indians dataset
-dataset = numpy.loadtxt("/home/info/pima-indians-diabetes.data", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
-# create model
-model = Sequential()
-model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
-model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# Fit the model
-history = model.fit(X, Y, validation_split=0.33, epochs=150, batch_size=10, verbose=1)
-
-# list all data in history
-print(history.history.keys())
 
 def generate_timestamp():
     timestring = time.strftime("%Y_%m_%d-%H_%M_%S")
@@ -35,10 +15,48 @@ def generate_timestamp():
 
 timestr = generate_timestamp()
 
+seed = 7                                                                        # Fix random seed for reproducibility
+numpy.random.seed(seed)
+
+dataset = numpy.loadtxt("/home/info/pima-indians-diabetes.data", delimiter=",") # load dataset
+
+X = dataset[:,0:8]                                                              # Split into input (X) and output (Y) variables
+Y = dataset[:,8]
+
+def save_model(model, file_loc):
+    file_pointer = os.path.join(file_loc[0]+"//trained_"+ timestr)
+    model.save_weights(os.path.join(file_pointer + ".model"))
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(file_pointer+"_model.json", "w") as json_file:
+        json_file.write(model_json)
+    print ("Saved the trained model weight to: " + 
+           str(os.path.join(file_pointer + ".model")))
+        
+
+model = Sequential()                                                            # Create model
+model.add(Dense(12, input_dim=8, 
+                kernel_initializer='uniform', activation='relu'))
+model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',                                       # Compile model
+              optimizer='adam', metrics=['accuracy'])                           
+
+history = model.fit(X, Y, validation_split=0.33, 
+                    epochs=150, batch_size=10, verbose=1)                       # Fit the model
+
+save_model(model,  ['/home/info'])
+
+# list all data in history
+print(history.history.keys())
+
 def plot_training(history):
   output_loc = '/home/info/'
-  output_file_acc = os.path.join(output_loc+"//training_plot_acc_"+timestr+".png")
-  output_file_loss = os.path.join(output_loc+"//training_plot_loss_"+timestr+".png")
+  output_file_acc = os.path.join(output_loc+"//training_plot_acc_"+
+                                 timestr + ".png")
+  output_file_loss = os.path.join(output_loc+"//training_plot_loss_"+
+                                  timestr +".png")
   
   fig_acc = plt.figure()
   plt.plot(history.history['acc'])
@@ -48,7 +66,8 @@ def plot_training(history):
   plt.xlabel('epoch')
   plt.legend(['train', 'test'], loc='upper left')
   fig_acc.savefig(output_file_acc, dpi=fig_acc.dpi)
-  print ("Successfully created model training accuracy plot...")
+  print ("Successfully created model training accuracy plot: " + 
+         str(output_file_acc))
   plt.close()
 
   fig_loss = plt.figure()
@@ -59,8 +78,8 @@ def plot_training(history):
   plt.xlabel('epoch')
   plt.legend(['train', 'test'], loc='upper left')
   fig_loss.savefig(output_file_loss, dpi=fig_loss.dpi)
-  print ("Successfully created model training loss function plot...")
-  plt.close()
+  print ("Successfully created model training loss function plot: " + 
+         str(output_file_loss))
+  plt.close()  
   
 plot_training(history)
-  
--- Machine_Vision/01_Transfer_Learning/src/transfer_learning.py
+++ Machine_Vision/01_Transfer_Learning/src/transfer_learning.py
@@ -2,7 +2,6 @@
 # Written by Rahul Remanan and MOAD (https://www.moad.computer) machine vision team.
 # For more information contact: info@moad.computer
 # License: MIT open source license (https://github.com/rahulremanan/python_tutorial/blob/master/LICENSE).
-# Example usage: python3 transfer_learning.py --training_directory /home/info/train --validation_directory /home/info/val --batches 20 --epochs 200 --output_directory /home/info --train_model True --fine_tune True --test_augmentation False --plot True --summary True
 import time
 import os
 import sys
@@ -17,11 +16,13 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD, RMSprop, Adagrad
+import h5py
+import json
 
-IM_WIDTH, IM_HEIGHT = 299, 299 #Fixed size for InceptionV3
+IM_WIDTH, IM_HEIGHT = 299, 299                                                  # Fixed input image size for Inception version 3
 DEFAULT_EPOCHS = 100
-DEFAULT_BATCHES = 10
-FC_SIZE = 128
+DEFAULT_BATCHES = 20
+FC_SIZE = 1024
 NB_LAYERS_TO_FREEZE = 169
 
 sgd = SGD(lr=1e-7, decay=0.5, momentum=1, nesterov=True)
@@ -31,29 +32,36 @@ optimizer = sgd
 
 def generate_timestamp():
     timestring = time.strftime("%Y_%m_%d-%H_%M_%S")
-    print ("Time stamp generated: "+timestring)
+    print ("Time stamp generated: " + timestring)
     return timestring
 
 timestr = generate_timestamp()
 
 def is_valid_file(parser, arg):
     if not os.path.isfile(arg):
-        parser.error("The file %s does not exist..." % arg)
+        parser.error("The file %s does not exist ..." % arg)
     else:
         return arg
     
 def is_valid_dir(parser, arg):
     if not os.path.isdir(arg):
-        parser.error("The folder %s does not exist..." % arg)
+        parser.error("The folder %s does not exist ..." % arg)
     else:
         return arg
  
-def get_bool_fn(bool_fn):       #Boolean filter
+def get_bool_fn(bool_fn):                                                       # Boolean filter
     try:
         load_value = bool(bool_fn)
         return load_value
     except:
-        print ("Please check if: --train_model, --load_model, --fine_tune, --test_aug, --plot arguments are True or False statements...")
+        print (("Please check if: \
+            --train_model \
+            --load_model \
+            --fine_tune \
+            --test_aug \
+            --plot \
+            --summary \
+            arguments are True or False statements..."))
         print ("Example usage: --train_model True ")
 
 def get_nb_files(directory):
@@ -68,29 +76,40 @@ def get_nb_files(directory):
 def setup_to_transfer_learn(model, base_model, optimizer):
   for layer in base_model.layers:
     layer.trainable = False
-  model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+  model.compile(optimizer=optimizer, 
+                loss='categorical_crossentropy', metrics=['accuracy'])
   return model
 
-def add_new_last_layer(base_model, nb_classes):
-  """Add last layer to the convnet"""
+def add_new_last_layer(base_model, nb_classes):                                 # Add the fully connected convolutional neural network layer
   x = base_model.output
   x = GlobalAveragePooling2D()(x)
-  x = Dense(FC_SIZE, activation='relu')(x) #new FC layer, random init
-  predictions = Dense(nb_classes, activation='softmax')(x) #new softmax layer
+  x = Dense(FC_SIZE, activation='relu')(x)                                      # New fully connected layer, random init
+  predictions = Dense(nb_classes, activation='softmax')(x)                      # New softmax layer
   model = Model(inputs=base_model.input, outputs=predictions)
   return model
 
-def setup_to_finetune(model, optimizer):
-  """Freeze the bottom NB_IV3_LAYERS and retrain the remaining top layers"""
+def setup_to_finetune(model, optimizer):                                        # Freeze the bottom NB_LAYERS and retrain the remaining top layers
   for layer in model.layers[:NB_LAYERS_TO_FREEZE]:
      layer.trainable = False
   for layer in model.layers[NB_LAYERS_TO_FREEZE:]:
      layer.trainable = True
-  model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+  model.compile(optimizer=optimizer, loss='categorical_crossentropy', 
+                metrics=['accuracy'])
   return model
 
-def train(args):
-  """Use transfer learning and fine-tuning to train a network on a new dataset"""
+def save_model(model, file_loc):
+    file_pointer = os.path.join(file_loc+"//trained_"+ timestr)
+    model.save_weights(os.path.join(file_pointer + ".model"))
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(os.path.join(file_pointer+"_model.json", "w")) as json_file:
+        json_file.write(model_json)
+    print ("Saved the trained model weights to: " + 
+           str(os.path.join(file_pointer + ".model")))
+    print ("Saved the trained model weights as a json file to: " + 
+           str(os.path.join(file_pointer+"_model.json")))    
+
+def train(args):                                                                # Transfer learning and fine-tuning to train
   nb_train_samples = get_nb_files(args.train_dir[0])
   print ("Total number of training samples = " + str(nb_train_samples))
   nb_classes = len(glob.glob(args.train_dir[0] + "/*"))
@@ -110,7 +129,8 @@ def train(args):
   test_aug = get_bool_fn(args.test_aug[0])  
   
   if test_aug==True:
-      test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+      test_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
           rotation_range=30,
           width_shift_range=0.2,
           height_shift_range=0.2,
@@ -125,34 +145,33 @@ def train(args):
     batch_size=batch_size,
     class_mode='categorical')
 
-  validation_generator = test_datagen.flow_from_directory(
-    args.val_dir[0],
+  validation_generator = test_datagen.flow_from_directory(args.val_dir[0],
     target_size=(IM_WIDTH, IM_HEIGHT),
     batch_size=batch_size,
     class_mode='categorical')
   
-  base_model = InceptionV3(weights='imagenet', include_top=False) #include_top=False excludes final FC layer
+  base_model = InceptionV3(weights='imagenet', include_top=False)               # Model argument: include_top=False excludes the final FC layer
   model = add_new_last_layer(base_model, nb_classes)
-  print ("Inception model loaded...")
+  print ("Base model for transfer learning: Inception version 3 ...")
   
   model_summary = get_bool_fn(args.model_summary[0])
   
   if model_summary == True:
       print (model.summary())
   else:
-      print ("Loading Inception version 3 ...")
+      print ("Successfully loaded the model for training ...")
     
   load_model = get_bool_fn(args.load_model[0])
   
   fine_tune_model = get_bool_fn(args.fine_tune[0])
   
   if load_model == True:
-      print (args.model_file[0])
+      print ("Loading model weights from: " + str(args.model_file[0]))
       model = keras.models.load_model(args.model_file[0])
-      print ("Loaded saved model weights...")
+      print ("Successfully loaded saved model weights ...")
   else:
       model = model
-      print ("Tabula rasa....")
+      print ("Tabula rasa ...")
       
   setup_to_transfer_learn(model, base_model, optimizer)
             
@@ -162,6 +181,7 @@ def train(args):
                   validation_data=validation_generator,
                   validation_steps=nb_val_samples // batch_size,
                   class_weight='auto')
+  
   setup_to_finetune(model, optimizer)
 
   model_ft = model.fit_generator(train_generator,
@@ -173,11 +193,13 @@ def train(args):
   
   if fine_tune_model == True:
       model_train = model_ft
+      save_model(model,  args.output_dir[0])
   else:
       model_train = model_tl
-      
+      save_model(model,  args.output_dir[0])
+            
   return model_train
-  
+
 def generate_plot(args, model_train):
     gen_plot = get_bool_fn(args.plot[0])
     if gen_plot==True:
@@ -187,8 +209,11 @@ def generate_plot(args, model_train):
 
 def plot_training(history):
   output_loc = args.output_dir[0]
-  output_file_acc = os.path.join(output_loc+"//training_plot_acc_"+timestr+".png")
-  output_file_loss = os.path.join(output_loc+"//training_plot_loss_"+timestr+".png")
+  
+  output_file_acc = os.path.join(output_loc+
+                                 "//training_plot_acc_"+timestr+".png")
+  output_file_loss = os.path.join(output_loc+
+                                  "//training_plot_loss_"+timestr+".png")
   
   fig_acc = plt.figure()
   plt.plot(history.history['acc'])
@@ -198,7 +223,8 @@ def plot_training(history):
   plt.xlabel('epoch')
   plt.legend(['train', 'test'], loc='upper left')
   fig_acc.savefig(output_file_acc, dpi=fig_acc.dpi)
-  print ("Successfully created model training accuracy plot...")
+  print ("Successfully created the training accuracy plot: " 
+         + str(output_file_acc))
   plt.close()
 
   fig_loss = plt.figure()
@@ -209,107 +235,126 @@ def plot_training(history):
   plt.xlabel('epoch')
   plt.legend(['train', 'test'], loc='upper left')
   fig_loss.savefig(output_file_loss, dpi=fig_loss.dpi)
-  print ("Successfully created model training loss function plot...")
+  print ("Successfully created the loss function plot: " 
+         + str(output_file_loss))
   plt.close()
   
 def get_user_options():
     a = argparse.ArgumentParser()
+    
     a.add_argument("--training_directory", 
-                 help = "Specify folder contraining the files for training...", 
+                 help = "Specify folder contraining the training files ...", 
                  dest = "train_dir", 
                  required = True, 
                  type=lambda x: is_valid_dir(a, x), 
                  nargs=1)
+    
     a.add_argument("--validation_directory", 
-                   help = "Specify folder containing the files for validation...", 
+                   help = "Specify folder containing the validation files ...", 
                    dest = "val_dir", 
                    required = True, 
                    type=lambda x: is_valid_dir(a, x), 
                    nargs=1)
+    
     a.add_argument("--epochs", 
-                   help = "Specify epochs for training...", 
+                   help = "Specify epochs for training ...", 
                    dest = "epoch", 
                    default=[DEFAULT_EPOCHS], 
                    required=False, 
                    type = int, 
                    nargs=1)
-    a.add_argument("--batches", help = "Specify batches for training...", 
+    
+    a.add_argument("--batches", help = "Specify batches for training ...", 
                    dest = "batch", 
                    default=[DEFAULT_BATCHES], 
                    required=False, 
                    type = int, 
                    nargs=1)
+    
     a.add_argument("--model_file", 
-                   help = "Specify pre-trained model file for training...", 
+                   help = "Specify pre-trained model file for training ...", 
                    dest = "model_file", 
                    default=["/home/info/model/inceptionv3-ft.model"], 
                    required=False, 
                    nargs=1)
+    
     a.add_argument("--output_directory", 
-                   help = "Specify output folder...", 
+                   help = "Specify output folder ...", 
                    dest = "output_dir", 
                    required = True, 
                    type=lambda x: is_valid_dir(a, x),
                    nargs=1)
+    
     a.add_argument("--train_model", 
-                   help = "Specify if the model should be trained...", 
+                   help = "Specify if the model should be trained ...", 
                    dest = "train_model", 
                    required=True, 
                    default=[True], 
                    nargs=1, 
                    type = bool)
+    
     a.add_argument("--load_model", 
-                   help = "Specify if a pre-trained model should be loaded...", 
+                   help = "Specify if pre-trained model should be loaded ...", 
                    dest = "load_model", 
                    required=False, 
                    default=[False], 
                    nargs=1, 
                    type = bool)
+    
     a.add_argument("--fine_tune", 
-                   help = "Specify model should be fine tuned...", 
+                   help = "Specify model should be fine tuned ...", 
                    dest = "fine_tune", 
                    required=False, 
                    default=[True], 
                    nargs=1, 
                    type = bool)
+    
     a.add_argument("--test_augmentation", 
-                   help = "Specify if image augmentation should be done on test dataset...", 
+                   help = "Specify image augmentation for test dataset ...", 
                    dest = "test_aug", 
                    required=False, 
                    default=[False], 
                    nargs=1, 
                    type = bool)
+    
     a.add_argument("--plot", 
-                   help = "Specify if a plot should be generated...", 
+                   help = "Specify if a plot should be generated ...", 
                    dest = "plot", 
                    required=False, 
                    default=[True], 
                    nargs=1, 
                    type = bool)
+    
     a.add_argument("--summary", 
-                   help = "Specify if a summary should be generated...", 
+                   help = "Specify if a summary should be generated ...", 
                    dest = "model_summary", 
                    required=False, 
                    default=[False], 
                    nargs=1, 
                    type = bool)
+    
     args = a.parse_args()
+    
     return args
 
 if __name__=="__main__":
     args = get_user_options()
 
-    if (not os.path.exists(args.train_dir[0])) or (not os.path.exists(args.val_dir[0])) or (not os.path.exists(args.output_dir[0])):
-      print("Specified directories do not exist...")
+    if ((not os.path.exists(args.train_dir[0])) 
+        or 
+    (not os.path.exists(args.val_dir[0])) 
+        or 
+    (not os.path.exists(args.output_dir[0]))):
+      print("Specified directories do not exist ...")
       sys.exit(1)
     train_model = get_bool_fn(args.train_model[0])  
     
     if train_model ==True:
-        print ("Training sesssion initiated...")
+        print ("Training sesssion initiated ...")
         train(args)
         generate_plot(args, train(args))
     else:
-        print ("Nothing to do here...")
-        print ("Try setting the --train_model flag to True...")
+        print ("Nothing to do here ...")
+        print ("Try setting the --train_model flag to True ...")
         print ("For more help, run with -h ...")
         sys.exit(1)
