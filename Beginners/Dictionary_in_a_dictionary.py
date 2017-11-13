2f089bd76d5a943307f4e28b69caecdcd7c0eb55
rahulremanan <rahul@remanan.net>
2017-11-12 23:17:56 -0500

    Updated prime factorization code

--- Beginners/Dictionary_in_a_dictionary.py
+++ Beginners/Dictionary_in_a_dictionary.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-grocery_list = {'Fish': [{"Salmon": 10, "Cuttle Fish": 10}],
-                'Apples': [{"Red Apple" : 100, "Granny Smith" : 300}],
-                'Milk': [{"Low Fat": 500, "Whole Milk": 500}],
-                'Eggs': [{"Organic White": 100, "Organic Brown": 100}],
-                'Chicken': [{"Organic Whole Chicken": 50, "Deboned chicken": 50}]}
+grocery_list = {'Fish in kilograms': [{"Salmon": 10, "Cuttle Fish": 10, "Tuna": 10}],
+                'Apples in counts': [{"California Red Apple" : 100, "Granny Smith" : 100}],
+                'Milk in liters': [{"Low Fat Milk": 500, "Whole Milk": 500, "Low Fat Milk -- Lactaid": 150, "Whole Milk -- Lactaid": 150}],
+                'Eggs in counts': [{"Organic White Eggs": 100, "Organic Brown Eggs": 100}],
+                'Chicken in kilograms': [{"Organic Whole Chicken": 50, "Deboned Chicken": 50}]}
 
 for item in grocery_list:
+    print (item)
     for g in grocery_list[item]:
         print (g)
         for i in g:
--- Beginners/Prime_factorization.py
+++ Beginners/Prime_factorization.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 n = 600851475143
-i = 2
-while i * i < n:
-     while n % i == 0:
-         n = n / i
-     i = i + 1
-
-print (int(n))
+# n=int(input("Enter an integer:"))
+print("Factors are:")
+i=1
+while(i<=n):
+    k=0
+    if(n%i==0):
+        j=1
+        while(j<=i):
+            if(i%j==0):
+                k=k+1
+            j=j+1
+        if(k==2):
+            print(i)
+    i=i+1
