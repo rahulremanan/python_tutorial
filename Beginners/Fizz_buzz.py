472606cc212bbdbb1b4ba0402615e6d99dd6b1af
rahulremanan <rahul@remanan.net>
2017-11-13 18:56:51 -0500

    Store fizz buzz output in a list

--- Beginners/Fizz_buzz.py
+++ Beginners/Fizz_buzz.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-
 start = int(input("Give me a start number :"))
 end = int(input("Give me an end number :"))
-            
           
 def fizz_buzz(start, end):
+    fb_state = []
     for num in range(start, end +1):
         if num % 3 == 0 and num % 5 == 0:
-            print(num, "fizzbuzz")
+            output = {"number": num, "fizz buzz state": "fizzbuzz"}
+            fb_state.append(output)
         elif num % 3 ==0:
-            print (num, "fizz")
+            output = {"number": num, "fizz buzz state": "fizz"}
+            fb_state.append(output)
         elif num % 5 ==0:
-            print (num, "buzz")
+            output = {"number": num, "fizz buzz state": "buzz"}
+            fb_state.append(output)
         else:
-            print (num, "nothing")
-    return None
+            output = {"number": num, "fizz buzz state": "none"}
+            fb_state.append(output)
+    return (fb_state)
 
-fizz_buzz(start, end)
+fb = (fizz_buzz(start, end))
+
+for item in fb:
+    print ("Number = ", item["number"], "; fizz buzz State = ", item["fizz buzz state"])
