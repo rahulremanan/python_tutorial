d188946fba22cc4373d58e9c0775fd32880d997d
rahulremanan <rahul@remanan.net>
2018-07-19 02:19:33 -0400

    Updated tensorflow installation instructions using CUDA 9.2 and cuDNN 7

--- Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
+++ Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
@@ -1,4 +1,5 @@
 ﻿import tensorflow as tf
+print(tf.__version__)
 with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
