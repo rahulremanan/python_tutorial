d89146eec5cea1c5dd6cdaa7e5198723b1e0df30
Rahul Remanan <rahul@remanan.net>
2020-02-16 09:47:45 +0530

    Updated to Tensorflow 2.x CPU and GPU matrix multiplication test

--- Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
+++ Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
@@ -1,9 +1,14 @@
 ﻿import tensorflow as tf
 print(tf.__version__)
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c_cpu = tf.matmul(a, b)
+
 with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
+    c_gpu = tf.matmul(a, b)
 
-with tf.Session() as sess:
-    print (sess.run(c))
+print ('Matrix multiplication result using CPU: {}   \
+        \nMatrix multiplication result using GPU: {}'.format(c_cpu, c_gpu))
