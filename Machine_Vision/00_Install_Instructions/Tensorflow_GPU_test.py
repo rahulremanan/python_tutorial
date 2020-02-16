d79e6025ece143b5a3acdfa84ff7acf85365e098
Rahul Remanan <rahul@remanan.net>
2020-02-16 15:54:35 +0530

    Added new tests for Tensorflow installation

--- Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
+++ Machine_Vision/00_Install_Instructions/Tensorflow_GPU_test.py
@@ -3,15 +3,24 @@
 print('Using tensorflow version: {} ...'.format(tf.__version__))
 print('Visible devices for tensorflow: {} ...'.format(tf.config.list_physical_devices()))
 
+print('Running tensorflow test: 1 ...')
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+
+print('Running a tensorflow test: 2 ... ')
+
 with tf.device('/cpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c_cpu = tf.matmul(a, b)
 
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c_gpu = tf.matmul(a, b)
+c_gpu = ['Failed ...']
+try:
+    with tf.device('/gpu:0'):
+        a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+        b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+        c_gpu = tf.matmul(a, b)
+except:
+    print('Tensorflow test using GPU failed ...')
 
 print ('Matrix multiplication result using CPU: {}   \
         \nMatrix multiplication result using GPU: {}'.format(c_cpu, c_gpu))
