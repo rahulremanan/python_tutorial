1c7f2fbd37bdc97e3f5b0f8755b3135fa0fe4a94
Rahul Remanan <rahul@remanan.net>
2020-02-16 10:37:33 +0530

    Minor housekeping while updating to Tensorflow 2.x

--- Machine_Vision/06_RaspberryPi_Integration/install/Aarch64_Tensorflow_test.py
+++ Machine_Vision/06_RaspberryPi_Integration/install/Aarch64_Tensorflow_test.py
@@ -1,4 +1,6 @@
-﻿import tensorflow as tf
+import subprocess
+print((subprocess.check_output("lscpu", shell=True).strip()).decode())
+import tensorflow as tf
 
 print('Using tensorflow version: {} ...'.format(tf.__version__))
 print('Visible devices for tensorflow: {} ...'.format(tf.config.list_physical_devices()))
@@ -8,10 +10,4 @@ with tf.device('/cpu:0'):
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c_cpu = tf.matmul(a, b)
 
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c_gpu = tf.matmul(a, b)
-
-print ('Matrix multiplication result using CPU: {}   \
-        \nMatrix multiplication result using GPU: {}'.format(c_cpu, c_gpu))
+print ('\nMatrix multiplication result using CPU: \n{}'.format(c_cpu))
